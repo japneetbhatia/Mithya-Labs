6ff1e596707c3314b1a74e517751f0c337be6390
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-12-28 11:06:53 +0530

    update

--- crud/src/App.js
+++ crud/src/App.js
@@ -5,12 +5,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      studentData : [],
-      act : 0,
-      index : ""
+      studentData: [],
+      act: 0,
+      index: ""
     }
   }
-  
+
   handleFormReloading = (e) => {
     e.preventDefault();
     let studentData = this.state.studentData;
@@ -18,7 +18,7 @@ class App extends Component {
     let enrollment = this.refs.textRoll.value;
     let cgpa = this.refs.textCGPA.value;
 
-    if(this.state.act === 0) {
+    if (this.state.act === 0) {
       let newStudent = {
         "name": name,
         "enrollment": enrollment,
@@ -30,7 +30,7 @@ class App extends Component {
       studentData[index].name = name;
       studentData[index].enrollment = enrollment;
       studentData[index].cgpa = cgpa;
-    }    
+    }
 
     this.setState({
       studentData: studentData,
@@ -41,16 +41,16 @@ class App extends Component {
   }
 
   handleEdit = (i) => {
-    let newStudent = this.state.studentData[i];  
+    let newStudent = this.state.studentData[i];
     // console.log(newStudent);
     this.refs.textName.value = newStudent.name;
     this.refs.textRoll.value = newStudent.enrollment;
     this.refs.textCGPA.value = newStudent.cgpa;
-    
+
     this.setState({
       // studentData: newStudent,
-      act : 1,
-      index : i
+      act: 1,
+      index: i
     })
   }
 
@@ -58,7 +58,7 @@ class App extends Component {
     let studentData = this.state.studentData;
     studentData.splice(i, 1);
     this.setState({
-      studentData : studentData
+      studentData: studentData
     });
     this.refs.textName.focus();
   }
@@ -66,42 +66,47 @@ class App extends Component {
   render() {
     let studentData = this.state.studentData;
     return (
-      <div className='container'>
-        <div className='table'>
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Enrollment No.</th>
-              <th>CGPA</th>
-            </tr>
-            {
-              studentData.map((newStudent, i) => 
-                <tr key={i}>
-                  <td>{newStudent.name}</td>
-                  <td>{newStudent.enrollment}</td>
-                  <td>{newStudent.cgpa}</td>
-                  <td>
-                    <button onClick={e => this.handleEdit(i)}>EDIT</button>
-                  </td>
-                  <td>
-                    <button onClick={e => this.handleDelete(i)}>DELETE</button>
-                  </td>
-                </tr>
-              )
-            }
-          </table>
+      <div class="main">
+        <div class="header">
+          <h2>CRUD APPLICATION (Create, Read, Update, Delete)</h2>
         </div>
-        <div className='form'>
-          <form ref="studentForm">
-            <label>Name</label>
-            <input type="text" ref="textName" placeholder="Enter your Name" />
-            <label>Enrollment No.</label>
-            <input type="text" ref="textRoll" placeholder="Enter your Enrollment" />
-            <label>CGPA</label>
-            <input type="text" ref="textCGPA" placeholder="Enter your CGPA" />
-            <button onClick={e => this.handleFormReloading(e)} className='button'>ADD</button>
-          </form>
-        </div>        
+        <div className='container'>
+          <div className='table'>
+            <table>
+              <tr>
+                <th>Name</th>
+                <th>Enrollment No.</th>
+                <th>CGPA</th>
+              </tr>
+              {
+                studentData.map((newStudent, i) =>
+                  <tr key={i}>
+                    <td>{newStudent.name}</td>
+                    <td>{newStudent.enrollment}</td>
+                    <td>{newStudent.cgpa}</td>
+                    <td>
+                      <button onClick={e => this.handleEdit(i)}>EDIT</button>
+                    </td>
+                    <td>
+                      <button onClick={e => this.handleDelete(i)}>DELETE</button>
+                    </td>
+                  </tr>
+                )
+              }
+            </table>
+          </div>
+          <div className='form'>
+            <form ref="studentForm">
+              <label>Name</label>
+              <input type="text" ref="textName" placeholder="Enter your Name" />
+              <label>Enrollment No.</label>
+              <input type="text" ref="textRoll" placeholder="Enter your Enrollment" />
+              <label>CGPA</label>
+              <input type="text" ref="textCGPA" placeholder="Enter your CGPA" />
+              <button onClick={e => this.handleFormReloading(e)} className='button'>ADD</button>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
